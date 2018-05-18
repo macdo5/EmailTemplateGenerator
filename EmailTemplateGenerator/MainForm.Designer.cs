d7acf5f40b464f3fe930ec4f828ba5f9d2e15d13
Oscar Macdonald <Oscar.Macdonald@sqlservices.com>
2018-05-18 22:08:05 +1200

    began working on UserControlLowDiskSpace to add to panelTemplate as a TEST

--- EmailTemplateGenerator/MainForm.Designer.cs
+++ EmailTemplateGenerator/MainForm.Designer.cs
@@ -34,15 +34,15 @@
             this.textBoxAdressee = new System.Windows.Forms.TextBox();
             this.comboBoxTemplates = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.panelTemplate = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(13, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.Size = new System.Drawing.Size(78, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Client code";
             // 
@@ -50,62 +50,68 @@
             // 
             this.comboBoxClients.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.comboBoxClients.FormattingEnabled = true;
-            this.comboBoxClients.Location = new System.Drawing.Point(10, 28);
-            this.comboBoxClients.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxClients.Location = new System.Drawing.Point(13, 34);
+            this.comboBoxClients.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxClients.Name = "comboBoxClients";
-            this.comboBoxClients.Size = new System.Drawing.Size(211, 21);
+            this.comboBoxClients.Size = new System.Drawing.Size(280, 24);
             this.comboBoxClients.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 58);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(12, 71);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.Size = new System.Drawing.Size(68, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Adressee";
             // 
             // textBoxAdressee
             // 
-            this.textBoxAdressee.Location = new System.Drawing.Point(9, 75);
-            this.textBoxAdressee.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxAdressee.Location = new System.Drawing.Point(12, 92);
+            this.textBoxAdressee.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxAdressee.Name = "textBoxAdressee";
-            this.textBoxAdressee.Size = new System.Drawing.Size(121, 20);
+            this.textBoxAdressee.Size = new System.Drawing.Size(160, 22);
             this.textBoxAdressee.TabIndex = 3;
             // 
             // comboBoxTemplates
             // 
             this.comboBoxTemplates.FormattingEnabled = true;
-            this.comboBoxTemplates.Location = new System.Drawing.Point(9, 124);
-            this.comboBoxTemplates.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxTemplates.Location = new System.Drawing.Point(12, 153);
+            this.comboBoxTemplates.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxTemplates.Name = "comboBoxTemplates";
-            this.comboBoxTemplates.Size = new System.Drawing.Size(212, 21);
+            this.comboBoxTemplates.Size = new System.Drawing.Size(281, 24);
             this.comboBoxTemplates.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 107);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(12, 132);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.Size = new System.Drawing.Size(67, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Template";
             // 
-            // Form1
+            // panelTemplate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.panelTemplate.Location = new System.Drawing.Point(13, 194);
+            this.panelTemplate.Name = "panelTemplate";
+            this.panelTemplate.Size = new System.Drawing.Size(1039, 418);
+            this.panelTemplate.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(798, 507);
+            this.ClientSize = new System.Drawing.Size(1064, 624);
+            this.Controls.Add(this.panelTemplate);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBoxTemplates);
             this.Controls.Add(this.textBoxAdressee);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBoxClients);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -121,6 +127,7 @@
         private System.Windows.Forms.TextBox textBoxAdressee;
         private System.Windows.Forms.ComboBox comboBoxTemplates;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panelTemplate;
     }
 }
 
---
+++ EmailTemplateGenerator/UserControlLowDiskSpace.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace EmailTemplateGenerator
+{
+    partial class UserControlLowDiskSpace
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxInstance = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Disk Drive:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Instance";
+            // 
+            // comboBoxInstance
+            // 
+            this.comboBoxInstance.FormattingEnabled = true;
+            this.comboBoxInstance.Location = new System.Drawing.Point(4, 25);
+            this.comboBoxInstance.Name = "comboBoxInstance";
+            this.comboBoxInstance.Size = new System.Drawing.Size(200, 24);
+            this.comboBoxInstance.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total disk drive capacity(GB):";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 145);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(84, 22);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Current free space(MB):";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(7, 207);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(86, 22);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // UserControlLowDiskSpace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxInstance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "UserControlLowDiskSpace";
+            this.Size = new System.Drawing.Size(1039, 418);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxInstance;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
